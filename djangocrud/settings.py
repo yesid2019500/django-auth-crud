8330fcac7399810111b6959656a55ce0cbf31e5d
Yessi Angulo <alexanderangulo917@gmail.com>
2023-01-17 08:24:46 -0500

    second up

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 }
 
